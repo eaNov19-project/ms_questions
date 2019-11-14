3a89bb99193d95c5406bb98e04d9d8ba2a526ba1
Ogabek Yuldashov <ogabek.yuldashov@gmail.com>
2019-11-14 10:22:40 -0600

    first commit

---
+++ src/main/java/ea/sof/ms_questions/MsQuestionsApplication.java
@@ -0,0 +1,13 @@
+package ea.sof.ms_questions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsQuestionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsQuestionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ea/sof/ms_questions/MsQuestionsApplicationTests.java
@@ -0,0 +1,13 @@
+package ea.sof.ms_questions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsQuestionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
