d29aae33da4890dd4c5538f7f100056ef3728102
Ogabek Yuldashov <ogabek.yuldashov@gmail.com>
2019-11-18 21:26:40 -0600

    Implemented AUTH circuit breaker

--- src/main/java/ea/sof/ms_questions/service/AuthServiceCircuitBreaker.java
+++ src/main/java/ea/sof/ms_questions/service/AuthServiceCircuitBreaker.java
@@ -1,9 +1,12 @@
 package ea.sof.ms_questions.service;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import ea.sof.ms_questions.controller.QuestionsController;
 import ea.sof.ms_questions.interfaces.AuthService;
 import ea.sof.shared.models.Response;
 import jdk.internal.jline.internal.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -14,13 +17,15 @@ public class AuthServiceCircuitBreaker {
     @Autowired
     AuthService authService;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthServiceCircuitBreaker.class);
+
     @HystrixCommand(fallbackMethod = "fallback")
     public ResponseEntity<Response> validateToken(String token){
         return authService.validateToken(token);
     }
 
     public ResponseEntity<Response> fallback(String token) {
-        Log.warn("AuthService is not available: validateToken fallback");
-        return ResponseEntity.ok(new Response());
+        LOGGER.warn("AuthService is not available: validateToken fallback");
+        return ResponseEntity.ok(new Response(false, "Authentication service is unavailable. Try later"));
     }
 }
