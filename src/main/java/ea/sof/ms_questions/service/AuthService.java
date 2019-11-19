1e70d9d145ecb432f477ad2900c50c0a7808d2cf
thanhtuyenle <ttle@mum.edu>
2019-11-18 21:24:58 -0600

    changed properties file

--- src/main/java/ea/sof/ms_questions/service/AuthService.java
+++ src/main/java/ea/sof/ms_questions/service/AuthService.java
@@ -3,7 +3,7 @@ package ea.sof.ms_questions.service;
 import ea.sof.shared.showcases.MsAuthShowcase;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name="authService", url = "${AUTHENTICATE_SERVICE}")
+@FeignClient(name="authService", url = "${authenticate.service}")
 //@FeignClient(name = "${feign.name}", url = "${feign.url}")
 public interface AuthService extends MsAuthShowcase {
 
