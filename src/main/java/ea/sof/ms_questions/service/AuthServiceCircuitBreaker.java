199fc3d6b9de92796a5fb36a47a20433d4f6210a
Ogabek Yuldashov <ogabek.yuldashov@gmail.com>
2019-11-18 21:28:17 -0600

    Implemented AUTH circuit breaker

--- src/main/java/ea/sof/ms_questions/service/AuthServiceCircuitBreaker.java
+++ src/main/java/ea/sof/ms_questions/service/AuthServiceCircuitBreaker.java
@@ -1,16 +1,13 @@
 package ea.sof.ms_questions.service;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import ea.sof.ms_questions.controller.QuestionsController;
 import ea.sof.ms_questions.interfaces.AuthService;
 import ea.sof.shared.models.Response;
-import jdk.internal.jline.internal.Log;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestHeader;
 
 @Service
 public class AuthServiceCircuitBreaker {
