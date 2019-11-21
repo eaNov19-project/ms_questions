f880bc1ef56a39cd41d271815fe24d0970f943ff
Ogabek Yuldashov <ogabek.yuldashov@gmail.com>
2019-11-20 21:17:37 -0600

    Adding comment to top answers of questions fixed

--- src/main/java/ea/sof/ms_questions/repository/QuestionRepository.java
+++ src/main/java/ea/sof/ms_questions/repository/QuestionRepository.java
@@ -2,6 +2,7 @@ package ea.sof.ms_questions.repository;
 
 import ea.sof.ms_questions.entity.QuestionEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,6 +11,9 @@ public interface QuestionRepository extends MongoRepository<QuestionEntity, Stri
     Optional<QuestionEntity> findById(String id);
     List<QuestionEntity> findByUserId(String id);
     List<QuestionEntity> findAllByActiveEqualsOrderByIdDesc(Integer active);
+
+    @Query("{ 'topAnswers.id' : ?0 }")
+    List<QuestionEntity> findAllByTopAnswers(String answerId);
 
 
 }
